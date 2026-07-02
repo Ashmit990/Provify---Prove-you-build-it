42593914318da8e9a0707d268d206cc46bfbdfb6
Ashmit <ashmit@example.com>
2026-07-02 19:37:25 +0545

    Update project files

--- backend/app/main.py
+++ backend/app/main.py
@@ -51,7 +51,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://provify.vercel.app",
+        "https://provify-prove-you-build-it.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
